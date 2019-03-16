e8042676969ff803f71ce56b51fda8a923b31672
Paszo <paszo2008@gmail.com>
2019-03-16 15:39:26 +0100

    [ADD] Added a course model

--- models/models.py
+++ models/models.py
@@ -2,6 +2,13 @@
 
 from odoo import models, fields, api
 
+class Course(models.Model):
+    _name = 'openacademy.course'
+    _description = "OpenAcademy Course"
+
+    name = fields.Char(string="Title", required=True)
+    description = fields.Text()
+
 # class openacademy(models.Model):
 #     _name = 'openacademy.openacademy'
 
