d9ae63d7b595a29300111742b6d79f8d2a876023
Paszo <paszo2008@gmail.com>
2019-03-17 19:49:05 +0100

    [ADD] Added the dashboard

--- __manifest__.py
+++ __manifest__.py
@@ -19,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'board'],
 
     # always loaded
     'data': [
@@ -29,6 +29,7 @@
         'views/templates.xml',
         'views/openacademy.xml',
         'views/partner.xml',
+        'views/session_board.xml',
         'reports.xml',
     ],
     # only loaded in demonstration mode
