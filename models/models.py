3f7d23c47ee528656af17696429f79bdf4ec2edc
Paszo <paszo2008@gmail.com>
2019-03-17 11:02:59 +0100

    [ADD] Added the computed fields

--- models/models.py
+++ models/models.py
@@ -24,12 +24,21 @@ class Session(models.Model):
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
 
-    instructor_id = fields.Many2one('res.partner', string="Instructor"
+    instructor_id = fields.Many2one('res.partner', string="Instructor",
         domain=['|', ('instructor', '=', True),
         ('category_id.name', 'ilike', "Teacher")])
     course_id = fields.Many2one('openacademy.course',
         ondelete='cascade', string="Course", required=True)
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
+    taken_seats = fields.Float(string="Taken seats", compute='_taken_seats')
+
+    @api.depends('seats', 'attendee_ids')
+    def _taken_seats(self):
+        for r in self:
+            if not r.seats:
+                r.taken_seats = 0.0
+            else:
+                r.taken_seats = 100.0 * len(r.attendee_ids) / r.seats
 
 # class openacademy(models.Model):
 #     _name = 'openacademy.openacademy'
